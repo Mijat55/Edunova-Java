0176a2590c581c35ed9026152fdebb93fa8750bf
Marko Mijatović <mijat5555@gmail.com>
2022-11-25 14:39:57 +0100

    dodavanje jos vjezbi iz vjezbe World

--- vjezba2.sql
+++ vjezba2.sql
@@ -25,3 +25,28 @@ and name like 'A%'
 or name like 'B%'
 and ID <=1 
 and ID >=10;
+
+#4 oznaci drzave sa manjim GNP od 1000 na kontinetu Azija
+select  * from country
+where GNP <1000
+and Continent ='Asia';
+
+
+#5 oznaci sve gradove koji pocinju sa A 
+select * from  city
+where Name like 'A%';
+
+
+#6 pronadji sve drzave koje se nalaze na zapadnoj hemisferi a da nisu u africi
+select * from country
+where region like 'West%'
+and Continent not like 'Africa%';
+
+#7 nadji sve gradove osima kabula
+select * from city
+where not (name='Kabul')
+
+#8 pronadji hrvatski jezik i gdje se prica
+select * from countrylanguage
+where `Language` like '%Croatian%'
+and CountryCode ='HRV';
